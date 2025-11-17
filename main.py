68a807ff347aaf05f596adf9c0224f44dbe16282
Brayan Sanchez <bsanchez100701@gmail.com>
2025-11-17 15:44:46 -0600

    Prototipo Panel Izquierdo Agregado

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
+import flet as ft
+import matplotlib as plt
+import calc
 
+
+def main(page: ft.Page):
+    def graficar(e):
+        pass
+    
+    page.title = 'Graficadora de funciones'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    lbl_min = ft.Text(value='X min: ', color='white')
+    txt_min = ft.TextField(value="0", width=100)
+
+    lbl_max = ft.Text(value='X max: ', color='white')
+    txt_max = ft.TextField(value="0", width=100)
+
+    btn_calc = ft.ElevatedButton(text='Calcular', on_click=graficar)
+    
+
+    dd_opt = ft.Dropdown(
+        alignment=ft.alignment.center,
+        width=100,
+        options=[
+            ft.dropdown.Option('sin'),
+            ft.dropdown.Option('cos'),
+            ft.dropdown.Option('tan')
+        ]
+    )
+
+
+    page.add(
+        ft.Column(
+            wrap=True,
+            spacing=20,
+            controls=[
+                lbl_min,
+                txt_min,
+                lbl_max,
+                txt_max,
+                btn_calc
+            ]
+        ),
+        ft.Column(
+        )
+    )
+
+
+if __name__ == '__main__':
+    ft.app(main)
